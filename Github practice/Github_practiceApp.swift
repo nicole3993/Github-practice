8828292fd466d8ae4c8d461f4e167e4549e461d2
Nicole <ice.cream4@icloud.com>
2023-06-12 08:23:24 -0700

    Initial Commit

---
+++ Github practice/Github_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_practiceApp.swift
+//  Github practice
+//
+//  Created by scholar on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
